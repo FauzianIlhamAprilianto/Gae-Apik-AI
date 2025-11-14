0b0f06f90e41776723ebe896738aa077b6e8ceaa
Fauzian <emailkamu@example.com>
2025-11-14 21:42:56 +0700

    hapus style.css dan perbaikan bug counter dan membuat reset streak

--- Counter.js
+++ Counter.js
@@ -4,6 +4,32 @@ let hari = String(now.getDate()).padStart(2, '0');
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 let minggu = days[now.getDay()];
 let lastReset = localStorage.getItem("lastReset");
+//localStorage.clear();
+
+// Tambah counter Kegiatan
+selesai.onclick = () => {
+    let counters = ['total', 'mingguan', 'harian'];
+    counters.forEach(key => {
+        let count = parseInt(localStorage.getItem(key)) || 0;
+        localStorage.setItem(key, (count + 1).toString());
+    });
+    // Tambah Streak
+    if(localStorage.getItem('bestStreak') == null){
+        localStorage.setItem('bestStreak', 0)
+        localStorage.setItem('streak', 0)
+        localStorage.setItem('streakHariIni', 0)
+    }
+    if(localStorage.getItem('streakHariIni') == 0){
+        let countStreak = parseInt(localStorage.getItem('streak') || 0)
+        localStorage.setItem('streak', (countStreak + 1).toString())
+        if(localStorage.getItem('bestStreak') < localStorage.getItem('streak')){
+            localStorage.setItem('bestStreak', localStorage.getItem('streak'))
+        }
+        localStorage.setItem('streakHariIni', 1)
+        localStorage.setItem('terakhir', hari)
+    }
+}
+
 
 // Reset harian
 if(localStorage.getItem('resetHarian') == null){
@@ -12,22 +38,18 @@ if(localStorage.getItem('resetHarian') == null){
 else if(localStorage.getItem('resetHarian') != hari){
     localStorage.setItem('harian', 0)
     localStorage.setItem('resetHarian', hari)
+    localStorage.setItem('streakHariIni', 0)
 }
 
-// Reset mingguan
-const MingguReset = localStorage.getItem('MingguReset')
+const mingguReset = localStorage.getItem('mingguReset')
 if(minggu == 'Mon'){
-    if(MingguReset !== now.toDateString()){
+    if(mingguReset !== now.toDateString()){
         localStorage.setItem('mingguan', 0)
-        localStorage.setItem("MingguReset", now.toDateString());
+        localStorage.setItem("mingguReset", now.toDateString());
     }
 }
 
-// Tambah counter Kegiatan
-    selesai.onclick = () => {
-        let counters = ['total', 'mingguan', 'harian', 'streak'];
-        counters.forEach(key => {
-            let count = parseInt(localStorage.getItem(key)) || 0;
-            localStorage.setItem(key, (count + 1).toString());
-        });
-    }
+// Reset Streak
+if(hari >= parseInt(localStorage.getItem('terakhir')) + 2 ){
+    localStorage.setItem('streak', 0)
+}
